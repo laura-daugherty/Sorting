a003223dd76bc22a2a016d2b357599a462ffa6ec
Laura Daugherty <lauradaugherty6@gmail.com>
2019-12-09 19:32:07 -0800

    selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,64 +1,28 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
-        for s in range(cur_index, len(arr)):
-            if arr[s] < arr[smallest_index]:
-                arr[smallest_index], arr[s] = arr[s], arr[smallest_index]
-    return arr
+        # swapped = True
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-# sorted = selection_sort(arr1)
-# print(sorted)
-
+        # (hint, can do in 3 loc)
+        # while swapped is True:
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
 
-
-# loop twice - 2 for loops, one for current index and 1 for smallest index
-    # if cur_index > smallest index, swap indexes
-    # if not leave in the same spot
-    # return arr
-    
-
-# loop twice, once for current index, one for current index +1
-# if current index > next index, swap spots
-# if not leave it
-# current index becomes current + 1, compares to the next one
-
-
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
+    return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
-    didSwap = True
-    while didSwap:
-        didSwap = False
-        for i in range(0, len(arr) -1):
-            cur_index = i
-            next_index = i + 1
-            if arr[cur_index] > arr[next_index]:
-                arr[cur_index], arr[next_index] = arr[next_index], arr[cur_index]
-                didSwap = True
-                
     return arr
-
-
-
-# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-#     #   [5, 8, 4, 2, 9, 6, 1, 3, 7, 0]
-# bubbleSorted = bubble_sort(arr1)
-# print(bubbleSorted)
-
-
-
-
-
-
 
 
 # STRETCH: implement the Count Sort function below
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -14,6 +14,40 @@ def merge_sort( arr ):
     return arr
 
 
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+
+
+merged = merge_sort(arr1)
+print(merged)
+
+
+
+# # Step 1: split into smaller lists
+# [5 9 3 7 2 8 1 6]
+# [5 9 3 7] [2 8 1 6]
+# [5 9] [3 7] [2 8] [1 6]
+# [5] [9] [3] [7] [2] [8] [1] [6]
+# # Step 2: merge
+# [5] [9] [3] [7] [2] [8] [1] [6]
+# [5 9] [3 7] [2 8] [1 6]
+# [3 5 7 9] [1 2 6 8]
+# [1 2 3 5 6 7 8 9]
+# To merge, walk along the two lists with two different indexes, and move the
+# smaller number into the destination list and advance that list's index. Repeat
+# until both lists are empty.
+
+
+
+
+
+
+
+
+
+
+
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
